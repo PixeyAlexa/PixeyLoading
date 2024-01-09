1e3fec95a4d0c0e56f068832ad66c648adbedcc5
PixeyAlexa <amilapushpakumara99@gmail.com>
2024-01-09 06:37:29 +0530

    V 1.0

---
+++ pixey-Loading/fxmanifest.lua
@@ -0,0 +1,16 @@
+fx_version 'cerulean'
+games { 'gta5' }
+
+description 'pixey-loading'
+version '1.3'
+author 'Huskyalexa'
+
+files {
+	'html.html',
+	'background.mp3',
+	'rain.mp4',
+}
+
+loadscreen {
+	'html.html'
+}
